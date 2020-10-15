4f8ad511d956f786d9cd851568cbe85bf2f12c6e
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-10-15 19:09:07 -0400

    First commit

---
+++ network/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    following = models.ManyToManyField("User", blank=True, related_name="followers")
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField(blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+
+    class Meta:
+        unique_together = ["post", "user"]
+
---
+++ network/tests.py
@@ -0,0 +1,62 @@
+from django.test import TestCase
+from .models import User, Post, Like
+import unittest
+from django.core.exceptions import ValidationError
+
+# Create your tests here.
+
+class NetworkTest(TestCase):
+
+    def setUp(self):
+        
+        #Create Users
+        u1 = User.objects.create(username="u1", password="1234")
+        u2 = User.objects.create(username="u2", password="1234")
+        u3 = User.objects.create(username="u3", password="1234")
+
+        #Create Posts
+        p1 = Post.objects.create(user=u1, content="Hello World")
+        p2 = Post.objects.create(user=u2, content="Hola Mundo")
+        p3 = Post.objects.create(user=u3, content="Bonjour le monde")
+
+    def test_created_posts(self):
+        u1 = User.objects.get(username="u1")
+        u2 = User.objects.get(username="u2")
+        u3 = User.objects.get(username="u3")
+        
+        self.assertEqual(u1.posts.count(), 1)
+        self.assertEqual(u2.posts.count(), 1)
+        self.assertEqual(u3.posts.count(), 1)
+
+    def test_like(self):
+        u1 = User.objects.get(username="u1")
+        u2 = User.objects.get(username="u2")
+        p1 = Post.objects.get(user=u1)
+        like = Like.objects.create(post=p1, user=u2)
+
+        self.assertEqual(p1.likes.count(), 1)
+
+    def test_valid_likes(self):
+        u1 = User.objects.get(username="u1")
+        u2 = User.objects.get(username="u2")
+        p1 = Post.objects.get(user=u1)
+        like = Like.objects.create(post=p1, user=u2)
+
+        self.assertRaises(ValidationError)
+
+    def test_followers(self):
+        u1 = User.objects.get(username="u1")
+        u2 = User.objects.get(username="u2")
+        u2.following.add(u1)
+
+        self.assertEqual(u1.followers.count(), 1)
+
+    def test_following(self):
+        u1 = User.objects.get(username="u1")
+        u2 = User.objects.get(username="u2")
+        u2.followers.add(u1)
+
+        self.assertEqual(u1.following.count(), 1)
+
+if __name__ == "__main__":
+    unittest.main()
